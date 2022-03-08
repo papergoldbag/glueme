6c98115429218ec2ba0772d593a7b5689e9fbcf6
arsen <sabarsenrash@gmail.com>
2022-03-09 00:58:38 +0500

    res

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,9 +11,7 @@ class Settings(BaseSettings):
     max_sec_code_reg: int
 
     class Config:
-        _env_file = pathlib.Path(os.path.abspath(__file__)).parent.parent.parent / '.env'
-        if os.path.exists(_env_file):
-            env_file = _env_file
+        env_file = pathlib.Path(os.path.abspath(__file__)).parent.parent.parent / '.env'
 
 
 settings: Settings = Settings()

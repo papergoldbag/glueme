e5b2b10261e1170a0210df001a68e33fc43f6cca
arsen <sabarsenrash@gmail.com>
2022-03-11 00:34:18 +0500

    res

--- src/app/api/routes/tags.py
+++ src/app/api/routes/tags.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.get('', response_model=list[TagOut])
-def all_tags(q: str = Query(...), user: models.User = Depends(get_current_user), s: Session = Depends(get_session)):
+def all_tags(user: models.User = Depends(get_current_user), s: Session = Depends(get_session)):
     return [TagOut.from_orm(t) for t in s.query(models.Tag).all()]
 
 

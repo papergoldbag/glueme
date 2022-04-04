9f64f748937ba35d8002b2e5b7206f89fcdb59c6
arsen <sabarsenrash@gmail.com>
2022-04-04 13:24:22 +0500

    res

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn src.utils.asgi:app --host=0.0.0.0 --port=${PORT} --workers 1
+web: python herokurun.py
---
+++ herokurun.py
@@ -0,0 +1,9 @@
+import os
+
+import uvicorn
+
+from glueme.app.settings import setup_from_env
+
+if __name__ == '__main__':
+    setup_from_env()
+    uvicorn.run('glueme.app.createapp:get_application', workers=1, port=os.getenv('PORT'), host='0.0.0.0')

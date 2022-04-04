3b0dfe1023f472bffe7418244042e1a4e9def4d8
arsen <sabarsenrash@gmail.com>
2022-04-04 23:29:13 +0500

    res

--- herokurun.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import uvicorn
-
-from glueme.app.settings import setup_from_env
-
-if __name__ == '__main__':
-    setup_from_env()
-    uvicorn.run('glueme.app.createapp:app', workers=1, port=os.getenv('PORT'), host='0.0.0.0')
